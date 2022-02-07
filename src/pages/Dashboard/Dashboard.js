157766d33832eb4dd940b1f71bba6e7b6c25f97b
unknown <jarce.boukoro@korian.fr>
2022-02-07 10:19:05 +0100

    second

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -1,5 +1,5 @@
 import React, {useEffect, useState} from "react";
-import { Col, Row, Form, Card, Container, Alert, Button } from '@themesberg/react-bootstrap';
+import { Col, Row, Form, Card, Container, Alert, Button, Table } from '@themesberg/react-bootstrap';
 import { Link, useHistory } from 'react-router-dom';
 import { Routes } from "../../routes";
 import { getUserConnect, users } from "../../data/Users";
@@ -38,9 +38,28 @@ function logout(){
             </Col>
           </Row>
 
-          <Card className="mt-5 py-4">
+          <Card className="mt-5 p-4">
             <Row>
-              <Col md="8"></Col>
+              <Col md="6">
+                <h1>Users list</h1>
+                <Table className="mt-4">
+                  <thead>
+                    <tr>
+                      <th>Email</th>
+                      <th>Action</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {usersList.map((item, index) => 
+                       <tr key={index}>
+                        <td>{item.email}</td>
+                        <td></td>
+                      </tr>
+                    )}
+                  </tbody>
+                </Table>
+              </Col>
+              <Col md="2"></Col>
               <Col md="4">
                 <ResetPassword />
               </Col>
